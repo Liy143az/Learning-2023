3de222967a956ce9c9100216a196d6665e0da089
Liyaz Nagoori <naguriliyaz@gmail.com>
2023-06-15 13:13:11 +0530

    commiting Module-1

---
+++ MODULE-1/Day-2/Question-1.c
@@ -0,0 +1,69 @@
+/*1. Write a program to print exponent part of double variable in hexadecimal and binary format.
+lets say we have x = 0.7, the exponent in hex is 0x3FE and 0b01111111110
+You may use the below link to validate you output
+https://baseconvert.com/ieee-754-floating-point
+
+Topics to be covered
+- Pointers
+- Bitwise Operators
+
+
+2. Write a program to swap any type of data passed to an function.
+
+Topics to be covered
+- Pointers
+- Type Casting
+
+ 
+
+3. Write a program to sum every alternate elements of a given array starting for element 0
+For example, let's say we have a[5] = {10, 20, 30, 40, 50}, the result should be 10 + 30 + 50 = 90
+
+Topics to be covered
+- Arrays
+- Loops
+- Basic Operators
+
+ 
+
+4. Write a program to find total number of bits set in a given array.
+Say we have a array of 3 elements
+a[3] = {0x1, 0xF4, 0x10001};
+The total number of set bits in the given array is
+1 bit in 0x01, 5 in 0xF4, 2 in 0x10001, so the total is 1 + 5 + 2 = 8 bits
+
+
+5. Write a program to find total number of bits set in a given array.
+Say we have a array of 3 elements
+a[3] = {0x1, 0xF4, 0x10001};
+The total number of set bits in the given array is
+1 bit in 0x01, 5 in 0xF4, 2 in 0x10001, so the total is 1 + 5 + 2 = 8 bits
+
+Topics to be covered
+- Arrays
+- Loops
+- Bitwise Operators*/
+
+#include <stdio.h>
+
+void printExponent(double x) {
+    unsigned long long *ptr = (unsigned long long *)&x;
+    unsigned long long exponentMask = 0x7FF0000000000000ULL;
+    unsigned long long exponent = (*ptr & exponentMask) >> 52;
+
+    printf("Exponent in hexadecimal: 0x%llX\n", exponent);
+    
+    printf("Exponent in binary: 0b");
+    for (int i = 11; i >= 0; i--) {
+        unsigned long long bit = (exponent >> i) & 1;
+        printf("%llu", bit);
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(x);
+
+return 0;
+}
